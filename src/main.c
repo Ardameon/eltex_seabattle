bfebca44596735f2627c956c20eed551323cdeb6
Boris Egorov <jightuse@gmail.com>
2012-07-05 23:50:58 +0700

    Changing main.c to test draw functions.

--- src/main.c
+++ src/main.c
@@ -1,15 +1,30 @@
 #include <ncurses.h>
+#include <stdlib.h>
+#include "Field.h"
+#include "UI.h"
 
 static void initialize_curses(void);
 static void greeting(void);
+static struct Field *get_field(void);
 
 int main(int argc, char *argv[])
 {
+	struct Field *f = get_field();
+	int x;
+	int y;
 	initialize_curses();
-	greeting();
+	print_enemy((const struct Field *)f, 10, 10);
+	print_friendly((const struct Field *)f, 10, 10);
+	read_coords(&x, &y, 10);
 
 	endwin();
 	return 0;
+}
+
+static struct Field *get_field(void)
+{
+	struct Field *f = (struct Field *)malloc(sizeof(struct Field));
+	return f;
 }
 
 static void initialize_curses(void)
@@ -18,14 +33,4 @@ static void initialize_curses(void)
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
-}
-
-static void greeting(void)
-{
-	char *msg = "Seabattle";
-
-	box(stdscr, 0, 0);
-	mvprintw(1, 1, msg);
-	refresh();
-	getch();
 }
