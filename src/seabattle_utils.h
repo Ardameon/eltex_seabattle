e02484ab7248afcce577cd1d71187e1ab3bde554
Vendigo <system.out@yandex.ru>
2012-07-06 00:12:14 +0700

    add prototipes of create_player() and create_players() functions to seabattle_utils.h

--- src/seabattle_utils.h
+++ src/seabattle_utils.h
@@ -1,4 +1,28 @@
 <<<<<<< HEAD
+
+
+/* int create_player(struct Player*, struct Config*)
+ * takes 2 arguments:
+ * 1. Pointer to array(uninitialized) of (struct Player).
+ * 2. (struct Config) pointer, which defines field size and other game options.
+ * RETURNED VALUE:
+ * create_player() returns 0 on success. On error it returns a negative number
+ * error code. Error codes is(will be) defined at 'error_codes.h'
+ * */
+
+int create_player(struct Player *player, struct Config *config);
+
+/*
+ * int create_players(struct Player*, int, struct Config*)
+ * Takes almost the same arguments which takes create_players(), unless
+ * second integer argument - count.
+ * RETURNED VALUE:
+ * returns 0 on success. On error returns error codes defined
+ * at 'error_codes.h'
+ * */
+
+int create_players(struct Player *players, int count, struct Config *config);
+
 /* Function make a shot into a field with coordinates (x, y)
  * Checking if we shot the ship or not.
  * Redusing its health.
@@ -7,7 +31,7 @@
  * */
 int shot (struct Player*, int x, int y);
 
-/*Checking ships count 
+/*Checking ships count
  * Input parammetrs: Massive of structures Player
  * Output data: Returns index of winner.
  * */
