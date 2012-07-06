a6ca5dcdb3c1288c571de4128aedae4d9a372d99
pylesosik <helldiver@yandex.ru>
2012-07-07 02:47:24 +0700

    shot.c modified

--- src/seabattle_utils.h
+++ src/seabattle_utils.h
@@ -3,6 +3,7 @@
 #include "Field.h"
 #include "Config.h"
 #include "Player.h"
+#include "Ship.h"
 
 #define SHOT_ALREADY_ATTACKED -1
 #define SHOT_MISSED 0
@@ -13,7 +14,10 @@
 int emplace_ships(struct Field *map, struct Config *config_game);
 int create_player(struct Player **player, struct Config *config);
 int create_players(struct Player **players, int count, struct Config *config);
-int shot(struct Player *player, int x, int y);
+int shot(struct Player *player, int x, int y,
+	 int *destroyed_ship_left,
+	 int *destroyed_ship_top,
+	 enum orientation *orient);
 int check_winner(struct Player **player);
 int finalize_field(struct Player *player);
 int run_game(struct Player **players);
--- src/shot.c
+++ src/shot.c
@@ -1,9 +1,13 @@
 #include "seabattle_utils.h"
 #include "Player.h"
+#include "Ship.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-int shot(struct Player *player, int x, int y)
+int shot(struct Player *player, int x, int y,
+	 int *destroyed_ship_left,
+	 int *destroyed_ship_top,
+	 enum orientation *orient)
 {
 	struct Cell *cell = &(player->field->field[x][y]);
 	struct Ship *potential_dead_ship;
@@ -18,6 +22,11 @@ int shot(struct Player *player, int x, int y)
 
 		if (potential_dead_ship->health == 0) {
 			player->ships_count--;
+
+			(*destroyed_ship_top) = potential_dead_ship->top;
+			(*destroyed_ship_left) = potential_dead_ship->left;
+			(*orient) = potential_dead_ship->orient;
+			
 			ship_destruct(potential_dead_ship);
 			
 			return SHOT_SHIP_DESTROYED;
