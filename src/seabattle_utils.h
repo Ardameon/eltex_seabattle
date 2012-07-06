9d8a02a0d511b8cf7fbfb7eebf6a7c68feb9128f
pylesosik <helldiver@yandex.ru>
2012-07-06 18:51:42 +0700

    seabattle_utils.h fixed prototypes

--- src/seabattle_utils.h
+++ src/seabattle_utils.h
@@ -5,10 +5,10 @@
 #include "Player.h"
 
 int emplace_ships(struct Field *map, struct Config *config_game);
-int create_player(struct Player *player, struct Config *config);
-int create_players(struct Player *players, int count, struct Config *config);
+int create_player(struct Player **player, struct Config *config);
+int create_players(struct Player **players, int count, struct Config *config);
 int shot (struct Player*, int x, int y);
 int check_winner (struct Player*);
 int finalize_field(struct Player* gamer);
-int run_game(struct Player ** players);
+int run_game(struct Player **players);
 #endif
