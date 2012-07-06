c753596508479ca9d56229a289cd23fc539e8083
pylesosik <helldiver@yandex.ru>
2012-07-07 01:22:30 +0700

    cosmetic changes in check_winner

--- src/check_winner.c
+++ src/check_winner.c
@@ -1,11 +1,13 @@
 #include "seabattle_utils.h"
 
-int check_winner(struct Player **Player)
+int check_winner(struct Player **player)
 {
 	int i = 0;
 	for (i = 0; i < 2; i++) {
-		if (Player[i]->ships_count == 0) return (i+1)%2;
+		if (player[i]->ships_count == 0) 
+			return (i + 1 ) % 2;
 	}
+
 	return -1;
 }
 
