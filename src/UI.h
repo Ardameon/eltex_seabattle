1b2522be15a23f4f8391195e8652e7ea226d5b39
silent <silent@server.eltex-edu>
2012-07-06 18:50:19 +0700

    run_game modified, added print_winners prototype

--- src/UI.h
+++ src/UI.h
@@ -12,5 +12,6 @@ int print_field(int sx, int sy, int w, int h, int cw, int ch);
 int print_enemy(const struct Field *f);
 int print_friendly(const struct Field *f);
 int read_coords(int *x, int *y, const int height, const int width);
+int print_winner(struct Player *winner);
 
 #endif
--- src/run_game.c
+++ src/run_game.c
@@ -1,27 +1,39 @@
-int run_game()
+#include "seabuttle_utils.h"
+#include "Player.h"
+#include "Field.h"
+#include "UI.h"
+
+int run_game(struct Player **players)
 {
 	int shot_val;
 	int current_player_index = 0;
 	int shot_x, shot_y;
 	int winner_index = -1;
+	struct Player *current_player;
+	struct Field *current_player_field;
 
-	while (winner_index = -1) {
-		print_friendly();
-		print_enemy();
-		read_coords();
-		shot_val = shot();
+	while (winner_index == -1) {
+		current_player = players[current_player_index];
+		current_player_field = current_player->field;
+		print_friendly(current_player_field);
+		print_enemy(current_player_field);
+		read_coords(&shot_x, &shot_y, current_player_field->height,
+			current_player_field->width);
+		shot_val = shot(current_player, shot_x, shot_y);
 		
 		if (shot_val == 0) {
 			current_player_index = (current_player_index + 1) % 2;
 		} else if (shot_val == 1) {
-			winner_index = check_winner();
+			winner_index = check_winner(current_player);
 		} else { 
 			continue;
 		}
 
-		print_friendly();
-		print_enemy();
+		print_friendly(current_player_field);
+		print_enemy(current_player_field);
 	}
-			//вывод победителя
+
+	print_winner(players[winner_index]);
+
 	return 0;
 }
