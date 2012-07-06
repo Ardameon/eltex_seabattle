e3756d7c8ec1f3fdbdb98817d3edea2f80e48d29
pylesosik <helldiver@yandex.ru>
2012-07-06 16:57:36 +0700

    run_game.c style fixed

--- src/run_game.c
+++ src/run_game.c
@@ -1,24 +1,27 @@
 int run_game()
 {
-	int shot_val,
-		current_player_index = 0,
-		shot_x,
-		shot_y,
-		winner_index = -1;
+	int shot_val;
+	int current_player_index = 0;
+	int shot_x, shot_y;
+	int winner_index = -1;
 
-		while(winner_index = -1) {
-			print_friendly();
-			print_enemy();
-			read_coords();
-			shot_val = shot();
-			if(shot_val == 0)
-				// смена игрока
-			else if (shot_val == 1)
-				winner_index = check_winner();
-			else continue;
-			print_friendly();
-			print_enemy();
+	while (winner_index = -1) {
+		print_friendly();
+		print_enemy();
+		read_coords();
+		shot_val = shot();
+		
+		if (shot_val == 0) {
+			current_player_index = (current_player_index + 1) % 2;
+		} else if (shot_val == 1) {
+			winner_index = check_winner();
+		} else { 
+			continue;
 		}
+
+		print_friendly();
+		print_enemy();
+	}
 			//вывод победителя
 	return 0;
 }
