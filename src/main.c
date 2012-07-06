8271363a727f216635b87d26b3d528ea799282c3
Boris Egorov <jightuse@gmail.com>
2012-07-06 18:08:45 +0700

    main function cleaning

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include <ncurses.h>
 
 static void initialize_curses(void);
-static void greeting(void);
 
 int main(int argc, char *argv[])
 {
 	initialize_curses();
-	greeting();
 
 	endwin();
 	return 0;
@@ -18,14 +16,4 @@ static void initialize_curses(void)
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
-}
-
-static void greeting(void)
-{
-	char *msg = "Seabattle";
-
-	box(stdscr, 0, 0);
-	mvprintw(1, 1, msg);
-	refresh();
-	getch();
 }
