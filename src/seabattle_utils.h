97c6d86fd1780584e136372d2b5a548008aaac56
Vagapov Roman <vagapaev@gmail.ru>
2012-07-06 16:06:09 +0700

    modified:   seabattle_utils.h cut function generate_ships_number_per_rank, paste to Config.h add include Player.h

--- src/seabattle_utils.h
+++ src/seabattle_utils.h
@@ -2,12 +2,12 @@
 #define SEABATTLE_UTILS_H
 #include "Field.h"
 #include "Config.h"
+#include "Player.h"
 
 int emplace_ships(struct Field *map, struct Config *config_game);
 int create_player(struct Player *player, struct Config *config);
 int create_players(struct Player *players, int count, struct Config *config);
 int shot (struct Player*, int x, int y);
-int check_winner (struct Players*);
-int * generate_ships_number_per_rank(int max_ship_size);
+int check_winner (struct Player*);
 
 #endif
