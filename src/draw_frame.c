f53d5fb598e1feef612112e637d03506da1ec92a
silent <silent@server.eltex-edu>
2012-07-09 20:18:17 +0700

    FUUUUUCKYYYYEEEEAAAAAAHHHHHH

--- src/draw_frame.c
+++ src/draw_frame.c
@@ -7,16 +7,18 @@ int draw_frame(struct Player *player,
 	int left, int top, enum orientation orient)
 {
 	struct Field *field = player->field;
-	int x = left, y = top;
+	int width = player->field->width, height = player->field->height;
+	int x = top, y = left;
 	int x_inc = 0, y_inc = 0;
 	
 	if (orient == HORIZONTAL)
-		x_inc = 1;
-	else
 		y_inc = 1;
+	else
+		x_inc = 1;
 	
-	while (field->field[y][x].ship == field->dummy_ship) {
-		draw_frame_around_cell(player, y, x);
+	while (x < width && y < height &&
+	       field->field[x][y].ship == field->dummy_ship) {
+		draw_frame_around_cell(player, x, y);
 		
 		x += x_inc;
 		y += y_inc;
